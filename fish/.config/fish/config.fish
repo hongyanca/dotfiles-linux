9fd415332548ebbd160ebec37f1cb25670321357
Hong Yan <contact@yanhong.ca>
2025-01-10 19:58:32 -0700

    feat: add git installation to quick-install.sh

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,10 +15,10 @@ alias ll='ls -la'
 alias lla='ls -la'
 alias lt='ls --tree'
 
-# set -x _nvim_binary "/usr/bin/nvim"
-# alias nvim="$_nvim_binary"
+set -x _nvim_binary /usr/bin/nvim
+alias nvim="$_nvim_binary"
 # set -x NVIM_APPNAME nvim          # kickstart or other
-# set -x NVIM_APPNAME nvim-lazyvim  # LazyVim
+set -x NVIM_APPNAME nvim-lazyvim # LazyVim
 alias vi="nvim"
 alias vim="nvim"
 # Alt+E edit the current command line in an external editor.
@@ -61,4 +61,4 @@ if test -f "$HOME/.cargo/env.fish"
 end
 
 # Set $PATH
-set -x PATH $PATH $NPM_PACKAGES/bin $HOME/.local/bin $HOME/scripts
+set -x PATH $NPM_PACKAGES/bin $HOME/.local/bin $HOME/scripts $PATH
