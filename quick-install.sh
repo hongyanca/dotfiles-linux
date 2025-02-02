2ba5cce6fe41f3640c58a1080255155f9fce5772
Hong Yan <contact@yanhong.ca>
2025-02-02 01:08:56 -0700

    feat: add git installation to quick-install.sh

--- quick-install.sh
+++ quick-install.sh
@@ -1,5 +1,64 @@
 #!/usr/bin/env bash
 
+LINUX_DISTRO="unknown"
+get_distro() {
+  # Declare an associative array for distro mappings
+  declare -A distro_map
+  distro_map["rhel"]="rhel"
+  distro_map["almalinux"]="rhel"
+  distro_map["fedora"]="fedora"
+  distro_map["debian"]="debian"
+  distro_map["ubuntu"]="debian"
+  distro_map["arch"]="arch"
+
+  # Read ID value
+  ID=$(grep ^ID= /etc/os-release | cut -d= -f2 | tr -d '"')
+
+  # Read the first word of ID_LIKE directly
+  ID_LIKE=$(grep ^ID_LIKE= /etc/os-release | cut -d= -f2 | tr -d '"' | awk '{print $1}')
+
+  # Check if the ID exists in our map
+  if [[ -n "$ID" ]] && [[ -v distro_map["$ID"] ]]; then
+    LINUX_DISTRO="${distro_map[$ID]}"
+  else
+    # If ID is not in the map, check ID_LIKE
+    if [[ -n "$ID_LIKE" ]] && [[ -v distro_map["$ID_LIKE"] ]]; then
+      LINUX_DISTRO="${distro_map[$ID_LIKE]}"
+    fi
+    # If no match found, keep unknown
+  fi
+}
+get_distro
+
+# Basic packages: git wget curl tar jq unzip bzip2
+install_required_dependencies() {
+  # Install packages based on the LINUX_DISTRO value
+  if [[ $LINUX_DISTRO == "rhel" ]]; then
+    echo "Detected RHEL-based distribution. Using dnf to install packages."
+    sudo dnf upgrade --refresh -y
+    # Install basic packages.
+    sudo dnf install -y git wget curl tar jq unzip bzip2 yum-utils
+  elif [[ $LINUX_DISTRO == "fedora" ]]; then
+    echo "Detected Fedora-based distribution. Using dnf to install packages."
+    sudo dnf upgrade --refresh -y
+    # Install basic packages.
+    sudo dnf install -y git wget curl tar jq unzip bzip2 yum-utils
+  elif [[ $LINUX_DISTRO == "debian" ]]; then
+    echo "Detected Debian-based distribution. Using apt-get to install packages."
+    sudo apt-get update
+    sudo apt-get install -y git wget curl tar jq unzip bzip2
+  elif [[ $LINUX_DISTRO == "arch" ]]; then
+    echo "Detected Arch-based distribution. Using pacman to install packages."
+    sudo pacman -S --needed --noconfirm archlinux-keyring
+    sudo pacman -Syu
+    sudo pacman -S --needed --noconfirm git wget curl tar jq unzip bzip2 util-linux
+  else
+    echo "Unknown distro" >&2
+    exit 1
+  fi
+}
+install_required_dependencies
+
 echo "Clone the Repository..."
 
 rm -rf ~/.dotfiles
